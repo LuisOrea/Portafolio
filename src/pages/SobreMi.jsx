28aac6147ca007f807b541ce2ec38171870a0e7f
WichoDev <luisadrianmartinezorea@gmail.com>
2025-10-01 16:24:46 -0600

    Ajuste de nombre

--- src/pages/SobreMi.jsx
+++ src/pages/SobreMi.jsx
@@ -17,7 +17,7 @@ export default function SobreMi() {
         <h1 className="title">Sobre Mi</h1>
 
         <div className="container-description"><p className="description-about">
-          Luis Martínez, desarrollador de software de 25 años con 2 años de experiencia en el diseño y desarrollo de aplicaciones web modernas. He trabajado con tecnologías como React.js, JavaScript, Vue.js y LitElement, desarrollando componentes reutilizables, interfaces eficientes e integraciones con APIs para distintos entornos empresariales.
+          Mi nombre es Luis Adrian Martínez, desarrollador de software de 25 años con 2 años de experiencia en el diseño y desarrollo de aplicaciones web modernas. He trabajado con tecnologías como React.js, JavaScript, Vue.js y LitElement, desarrollando componentes reutilizables, interfaces eficientes e integraciones con APIs para distintos entornos empresariales.
         </p>
         <p className="description-about">
           Además de mi experiencia en frontend, tengo la capacidad de desenvolverme en otras áreas del desarrollo como backend, testing y soporte técnico, lo que me permite adaptarme a distintos roles según las necesidades del proyecto.
