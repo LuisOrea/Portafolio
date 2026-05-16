f84b04702509c57a2a5568ae8e176400e1b3d256
Luis Martinez <luisadrianmartinezorea@gmail.com>
2026-05-16 04:30:11 -0600

    Ajuste de nombre - v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RoutePages } from './routes/routes'
+import { RoutePages } from './Routes/Routes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
