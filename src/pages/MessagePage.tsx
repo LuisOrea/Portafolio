2cbcae3297b3e6cde7b5d2682cca743683c3b5f6
Luis Martinez <luisadrianmartinezorea@gmail.com>
2026-05-16 20:55:25 -0600

    Prueba de despliegue - v2.0

--- src/pages/MessagePage.tsx
+++ src/pages/MessagePage.tsx
@@ -4,7 +4,7 @@ import { Message } from "../components/Message"
 export  function MessagePage() {
     return(
         <div>
-            <Message mssg="En Reconstruccion" mssgX="Para mas informacion favor de comunicarse mediante correo: luisadrianmartinezorea@gmail.com o numero de telefono: 5548737525" />
+            <Message mssg="Hola desde dev" />
         </div>
     )
 }
